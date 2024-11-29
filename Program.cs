0164ea1c244ba144cf80dd34c6c746e36b1c39eb
janos1000 <birinyij@kkszki.hu>
2024-11-29 12:02:19 +0100

    Update method is added to project.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+using Org.BouncyCastle.Asn1.Cmp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Media;
+using System.Numerics;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +27,7 @@ namespace BasketTeam
 
             dr.Read();
 
-            do 
+            do
             {
                 var player = new
                 {
@@ -34,18 +37,111 @@ namespace BasketTeam
                     Weight = dr.GetInt32(3),
                     CreatedTime = dr.GetDateTime(4)
                 };
-            
 
-            Console.WriteLine($"Játékos adatok: {player.Name},{player.Height},{player.Weight}");
+
+                Console.WriteLine($"Játékos adatok: {player.Name},{player.Height},{player.Weight}");
             } while (dr.Read());
 
             dr.Close();
             conn.Connection.Close();
-            
+
+        }
+        public static void AddNewPlayer(string name, int height, int weight)
+        {
+            try
+            {
+                conn.Connection.Open();
+
+                string sql = $"INSERT INTO `player`(`Name`, `Height`, `Weight`) VALUES ('{name}',{height},{weight})";
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+                cmd.ExecuteNonQuery();
+
+                conn.Connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        public static void Deleteplayer(int id)
+        {
+            conn.Connection.Open();
+
+            string sql = $"DELETE FROM `player` WHERE `id`= {id};";
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+            cmd.ExecuteNonQuery();
+
+            conn.Connection.Close();
+        }
+        public static void UpdatePlayer(int id, string name, int height, int weight)
+        {
+            conn.Connection.Open();
+
+            string sql = $"UPDATE `player` SET ,`Name`='{name}',`Height`= {height},`Weight`= {weight} WHERE `id`= {id};";
+            MySqlCommand cmd = new MySqlCommand (sql, conn.Connection);
+            cmd.ExecuteNonQuery();
+            conn.Connection.Close();
         }
         static void Main(string[] args)
         {
-         GetAllData();
+            try
+            {
+                Console.Write("Kérem a játékos azonosítót: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Kérem az új nevet: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Kérem az új magasságát: ");
+                int height = int.Parse(Console.ReadLine());
+
+                Console.Write("Kérem az új súlyát: ");
+                int weight = int.Parse(Console.ReadLine());
+
+                UpdatePlayer(id, name, height, weight);
+                Console.WriteLine("Sikeres frissítés.");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            //try
+            //{
+            //    Console.WriteLine("Kérem a játékos azonosítót a törléshez: ");
+            //    int azon = int.Parse(Console.ReadLine());
+            //    Deleteplayer(azon);
+            //    Console.WriteLine("Sikeres törlés.");
+
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+
+
+            //GetAllData();
+
+            //try
+            //{
+            //    Console.Write("Kérem a játékos nevét: ");
+            //    string name = Console.ReadLine();
+            //    Console.Write("Kérem a játékos magasságát: ");
+            //    int height = int.Parse(Console.ReadLine());
+            //    Console.Write("Kérem a játékos súlyát: ");
+            //    int weight = int.Parse(Console.ReadLine());
+            //    AddNewPlayer(name, height, weight);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+
         }
     }
 }
